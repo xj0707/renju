fbfe44afb5c08ba73b25faf04521f1f1060b3666
xj <799137494@qq.com>
2018-11-15 00:21:34 +0800

    棋盘绘制完毕

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+
+let chessColor = true   //默认黑子
+let chessBoard = []       //初始化一个棋盘点
+for (let i = 0; i < 15; i++) {
+    chessBoard[i] = []
+    for (let j = 0; j < 15; j++) {
+        chessBoard[i][j] = 0
+    }
+}
 //绘制棋盘
 let chess = document.getElementById('chess')
 //获取上下文
@@ -8,8 +17,41 @@ context.strokeStyle = '#bfbfbf'
 let logo = new Image()
 logo.src = 'image/muye.png'
 logo.onload = function () {
-    context.drawImage(logo, 0, 0, 450, 450)
+    // context.drawImage(logo, 0, 0, 450, 450)  //水印图 目前注释掉 没有合适的图
     lineation()
+}
+//给棋盘绑定事件
+chess.onclick = function (e) {
+    let x = e.offsetX
+    let y = e.offsetY
+    let i = Math.floor(x / 30)
+    let j = Math.floor(y / 30)
+    if (chessBoard[i][j] == 0) { //没有占用可以落子
+        chessPieces(i, j, chessColor)
+        if (chessColor) {
+            chessBoard[i][j] = 1
+        } else {
+            chessBoard[i][j] = 2
+        }
+        chessColor = !chessColor   //交叉改变
+    }
+}
+//创建棋子
+function chessPieces(i, j, color) {
+    //画棋子（圆）
+    context.beginPath()  //开始
+    context.arc(15 + i * 30, 15 + j * 30, 13, 0, 2 * Math.PI) //圆心坐标，半径，起始幅度
+    context.closePath()  //结束
+    let gradient = context.createRadialGradient(15 + i * 30 + 2, 15 + j * 30 - 2, 13, 15 + i * 30 + 2, 15 + j * 30 - 2, 0) //生成渐变对象
+    if (color) {  //黑子
+        gradient.addColorStop(0, '#0a0a0a')
+        gradient.addColorStop(1, '#636766')
+    } else {  //白子
+        gradient.addColorStop(0, '#d1d1d1')
+        gradient.addColorStop(1, '#f9f9f9')
+    }
+    context.fillStyle = gradient  //设置颜色
+    context.fill() //绘制圆并填充
 }
 //划线
 function lineation() {
